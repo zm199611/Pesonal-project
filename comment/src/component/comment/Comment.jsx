a0db7d0a5be03d115ac8689f5ef7bac7d3a17e07
zhuming <1109282075@qq.com>
2020-08-05 09:50:40 +0800

    hook+styled-component编写一个评论组件

--- comment/src/component/comment/Comment.jsx
+++ comment/src/component/comment/Comment.jsx
@@ -3,30 +3,42 @@ import Style from './Style'
 import { renderRoutes } from 'react-router-config'
 import { UserName, Content, Button, Down, DownContent } from './Style'
 import { Globalstyle } from './Style'
+import { useState } from 'react'
 
- function Comment(props) {
+function Comment(props) {
     // const { route } = props
+    const { Username, setUsername } = useState('') 
+    const { Content, setContent } = useState('') 
+    const change = ((e)=>{
+        this.setUsername({
+            Username:e.target.value
+    })
+    
     return (
         <div className="all">
-            <div className="top">
-                <Globalstyle />
-                <UserName>
-                    <label htmlFor=""> 用户名<input type="text" className="zm" /></label>
-                </UserName>
-                <Content>
-                    <label htmlFor=""><textarea name="" id="" cols="30" rows="10"></textarea>评论内容</label>
-                </Content>
-                <Button>
-                    <button className="btn">发布</button>
-                </Button>
+            <div className="content">
+                <div className="top">
+                    <Globalstyle />
+                    <UserName>
+                        <label htmlFor="" className="name">用户名: <input type="text" className="zm" /></label>
+                    </UserName>
+                    <Content>
+                        <label htmlFor="" className="cont">评论内容:<textarea className="content1" id="" cols="30" rows="10"></textarea></label>
+                    </Content>
+                    <Button>
+                        <button className="btn">发布</button>
+                    </Button>
+                </div>
+                <div className="footer">
+                    <DownContent>
+                        <div className="speak"></div>
+                    </DownContent>
+                </div>
+                {/* {renderRoutes(route.routes)} */}
             </div>
-            <div className="footer">
-                <DownContent>
-                    <div className="speak"></div>
-                </DownContent>
-            </div>
-            {/* {renderRoutes(route.routes)} */}
+
         </div>
     )
 }
+
 export default React.memo(Comment)
--- comment/src/component/comment/Style.js
+++ comment/src/component/comment/Style.js
@@ -6,27 +6,49 @@ body{
 　　margin: 0;
 　　padding: 0;
     background-color:#FBFBFB;
-　}
->.all{
+}
+body .all{
     height:500px;
-    width:200px;
+    width:350px;
     background-color:#FFFFFF;
+    margin-left:50%;
+    margin-top:50%;
+    transform:translate3d(-50%,-50%,0)
+}
+body .all .content{
+    width:300px;
+    height:450px;
+    border: 1px solid #DDDDDD;
+    position: relative;
+    left:25px;
+    top:25px;
 }
 `
 export const UserName = styled.div`
-    margin-left:20px;
-    color:#0000;
-    display:flex;
+    left:-5px;
+    position: relative;
+    top:20px;
+    .zm{
+        width:230px;
+        height:30px;
+        border-radius:5px;
+        border:1px solid #DDDDDD;
+        margin-left:20px;
+        /* border-radius:3px; */
+
+    }
 `
 export const Content = styled.div`
-     color:#0000;
-    display:flex;
-    margin-left:70px;
-
+    position: relative;
+    left: -10px;
+    top:40px;
+    .content1{
+        border-radius:5px;
+        border:1px solid #DDDDDD;
+        margin-left:30px;
+    }
 `
 export const Button = styled.div`
-    
-    /* background-color:blue; */
     border-radius:10px;
 >.btn{
     border-radius:5px;
@@ -36,6 +58,8 @@ export const Button = styled.div`
     border:hidden;
     width:100px;
     height:30px;
+    margin-left:180px;
+    margin-top:80px;
 
 }
 
